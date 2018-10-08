b35189f9a21c3a35031fccf8661fb17d6bd01823
avryhof <amos@vryhofresearch.com>
2018-10-08 15:37:07 -0400

    Release 1.0.2 to fix PyPi problem I created with 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ambient_api',
-    version='1.0.1',
+    version='1.0.2',
     packages=['ambient_api'],
     url='https://github.com/avryhof/ambient_api',
     license='MIT',
