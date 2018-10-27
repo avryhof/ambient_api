d0f3ac2d02a1e81f8e637bcd6a5dcc33eb5f0b20
tmd224 <41244312+tmd224@users.noreply.github.com>
2018-10-27 11:13:49 -0400

    Adding support to pass in API keys as kwargs instead of environmental variables.
    Added error checking for invalid API key response code

--- ambient_api/settings.py
+++ ambient_api/settings.py
@@ -1,5 +1,8 @@
 import os
 
-AMBIENT_ENDPOINT = 'https://api.ambientweather.net/v1'
-AMBIENT_APPLICATION_KEY = os.environ['AMBIENT_APPLICATION_KEY']
-AMBIENT_API_KEY = os.environ['AMBIENT_API_KEY']
+try:
+    AMBIENT_ENDPOINT = 'https://api.ambientweather.net/v1'
+    AMBIENT_APPLICATION_KEY = os.environ['AMBIENT_APPLICATION_KEY']
+    AMBIENT_API_KEY = os.environ['AMBIENT_API_KEY']
+except KeyError:
+    pass
