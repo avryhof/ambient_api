cb79a71335d6ffa440c2358b113bfcef35b8fddf
Amos Vryhof <avryhof@gmail.com>
2024-06-20 06:11:14 -0400

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ambient_api",
-    version="1.5.6",
+    version="1.5.7",
     packages=["ambient_api"],
     url="https://github.com/avryhof/ambient_api",
     license="MIT",
